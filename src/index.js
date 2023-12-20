3c00fb1dabfd9e3b7cb605685fab82ca21191ea8
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-12-20 23:24:46 +0000

    js code added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', documentOnReady);
 
 function documentOnReady() {
-
+    const checkThemeEl = document.getElementById('check-theme');
+    checkThemeEl.addEventListener('change', function () {
+        const blackColor = "#151515";
+        const whiteColor = "#aaaaaa";
+        const styleObj = document.documentElement.style;
+        if (checkThemeEl.checked) {
+            styleObj.setProperty("--primary-color", whiteColor);
+            styleObj.setProperty("--contrast-color", blackColor);
+        } else {
+            styleObj.setProperty("--primary-color", blackColor);
+            styleObj.setProperty("--contrast-color", whiteColor);
+        }
+    })
 }
